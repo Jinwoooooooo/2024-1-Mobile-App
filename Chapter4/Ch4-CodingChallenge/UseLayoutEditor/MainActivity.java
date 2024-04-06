d8a1ff0d4456178efa2df5e4c191419abbbeb77b
진우 <128763219+Jinwoooooooo@users.noreply.github.com>
2024-04-06 23:36:47 +0900

    Add files via upload

---
+++ Chapter4/Ch4-CodingChallenge/UseLayoutEditor/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.uselayouteditor;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeBackgroundColor();
+            }
+        });
+    }
+
+    private void changeBackgroundColor() {
+        int color = generateRandomColor();
+        getWindow().getDecorView().setBackgroundColor(color);
+    }
+
+    private int generateRandomColor() {
+        Random random = new Random();
+        int red = random.nextInt(256);
+        int green = random.nextInt(256);
+        int blue = random.nextInt(256);
+        return Color.rgb(red, green, blue);
+    }
+}
